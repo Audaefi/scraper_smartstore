33be5835942a42377e6ea99dec15dabf0f934ba4
KJ.O <nomeansno@Live.ca>
2022-10-02 16:16:43 +0900

    Initial commit

---
+++ scraper/ns_metadata.py
@@ -0,0 +1,9 @@
+DRIVER_PATH = '/Users/audaefi/chromedriver/chromedriver'
+# '/usr/bin/chromedriver'
+
+IMG_SRC_CSS = ".thumbnail_thumb__Bxb6Z > img"
+HREF_CSS = ".basicList_link__JLQJf"
+PRODUCT_TITLE_CSS = ".basicList_link__JLQJf"
+PRICE_CSS = ".price_num__S2p_v"
+SELLER_CSS = ".basicList_mall__BC5Xu"
+NEXT_ARROW_BTN_CSS = ".pagination_next__pZuC6"
