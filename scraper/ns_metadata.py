cb5dcc994ed7cafd0ce7f24a03e5fcef81bc50fa
KJ.O <nomeansno@Live.ca>
2022-10-03 17:15:11 +0900

    several changes

--- scraper/ns_metadata.py
+++ scraper/ns_metadata.py
@@ -1,9 +1,9 @@
-#DRIVER_PATH = '/Users/audaefi/chromedriver/chromedriver'
-DRIVER_PATH = '/usr/bin/chromedriver'
+DRIVER_PATH = '/Users/audaefi/chromedriver/chromedriver'
+#DRIVER_PATH = '/usr/bin/chromedriver'
 
-IMG_SRC_CSS = ".thumbnail_thumb_wrap__RbcYO._wrapper > a > img"
-HREF_CSS = ".imgList_link__fZgUG"
-PRODUCT_TITLE_CSS = ".imgList_link__fZgUG"
+IMG_SRC_CSS = ".basicList_img_area__AdRY_ > div > a > img"
+HREF_CSS = ".basicList_img_area__AdRY_ > div > a"
+PRODUCT_TITLE_CSS = ".basicList_link__JLQJf"
 PRICE_CSS = ".price_num__S2p_v"
-SELLER_CSS = ".imgList_mall_area__E5kC7 > a > em"
+SELLER_CSS = ".basicList_mall__BC5Xu"
 NEXT_ARROW_BTN_CSS = ".pagination_next__pZuC6"
